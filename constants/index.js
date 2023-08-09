e54f657fb8dce4d5e3e50803d4876f346b3eb66e
jamal moslem <139969578+jomhorikiri45@users.noreply.github.com>
2023-08-09 01:05:56 -0800

    Update index.js

--- constants/index.js
+++ constants/index.js
@@ -1,4 +1,4 @@
-export const TOKEN_CONTRACT_ADDRESS = "0xfF5cC26d33A84F10760718b207B1765744D0fB0A";
+export const TOKEN_CONTRACT_ADDRESS = "0xe1f00e1dada937661f55b1d61d1426e0ab945d2d";
 export const TOKEN_CONTRACT_ABI = [
 	{
 		"anonymous": false,
